c24a9878b6314d48ad417f40f777b59895662a4c
Luis Yauri <luis.yauri159@gmail.com>
2025-06-03 21:01:36 -0500

    feat: add responsive grid and validation to hypertension evaluation form inputs

--- src/app/features/hypertension-risk/evaluation/evaluation.component.ts
+++ src/app/features/hypertension-risk/evaluation/evaluation.component.ts
@@ -1,10 +1,48 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { TitleComponent } from "../../../shared/components/title/title.component";
 import { NzAlertModule } from "ng-zorro-antd/alert";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzInputNumberModule } from "ng-zorro-antd/input-number";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzButtonModule } from "ng-zorro-antd/button";
+
 @Component({
   selector: "app-evaluation",
-  imports: [TitleComponent, NzAlertModule],
+  imports: [
+    TitleComponent,
+    NzAlertModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzGridModule,
+    NzButtonModule,
+  ],
   templateUrl: "./evaluation.component.html",
   styleUrl: "./evaluation.component.scss",
 })
-export class EvaluationComponent {}
+export class EvaluationComponent {
+  private fb = inject(NonNullableFormBuilder);
+  validateForm = this.fb.group({
+    totalActivity: this.fb.control("", [Validators.required]),
+    bodyMass: this.fb.control("", [Validators.required]),
+    bloodPressure: this.fb.control("", [Validators.required]),
+    averageWeight: this.fb.control("", [Validators.required]),
+    averageHeight: this.fb.control("", [Validators.required]),
+  });
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log("submit", this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
